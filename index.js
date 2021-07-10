3f93c3c0258e3baffb792548f0ed0be573f89deb
Jo du Plessis <jo@joduplessis.com>
2021-07-10 06:32:42 +0200

    wip

--- index.js
+++ index.js
@@ -102,9 +102,12 @@ Toolkit.run(async (tools) => {
 
     // GIT logic
     try {
-        const current = pkg.version.toString()
-
-        console.log('Current package version: ', current)
+        const currentVersion = pkg.version.toString()
+        const currentVersionParts = currentVersion.split('-')
+        const calVersion = currentVersionParts[0]
+        const patchVersion = currentVersionParts[1]
+        const bumpedPatchVersion = patchVersion ? Number(patchVersion) + 1 : 0
+        const newVersion = calVersion + '-' + bumpedPatchVersion
 
         // set git user
         await tools.runInWorkspace('git', [
@@ -112,6 +115,7 @@ Toolkit.run(async (tools) => {
             'user.name',
             `"${process.env.GITHUB_USER || 'Automated Version Bump'}"`,
         ])
+
         await tools.runInWorkspace('git', [
             'config',
             'user.email',
@@ -121,21 +125,18 @@ Toolkit.run(async (tools) => {
         let currentBranch = /refs\/[a-zA-Z]+\/(.*)/.exec(process.env.GITHUB_REF)[1]
         let isPullRequest = false
         if (process.env.GITHUB_HEAD_REF) {
-            // Comes from a pull request
             currentBranch = process.env.GITHUB_HEAD_REF
             isPullRequest = true
         }
+
         if (process.env['INPUT_TARGET-BRANCH']) {
-            // We want to override the branch that we are pulling / pushing to
             currentBranch = process.env['INPUT_TARGET-BRANCH']
         }
+
         console.log('currentBranch:', currentBranch)
-        // do it in the current checked out github branch (DETACHED HEAD)
-        // important for further usage of the package.json version
         await tools.runInWorkspace('npm', ['version', '--allow-same-version=true', '--git-tag-version=false', current])
         console.log('current:', current, '/', 'version:', version)
-        let newVersion = execSync(`npm version --git-tag-version=false ${version}`).toString().trim().replace(/^v/, '')
-        newVersion = `${tagPrefix}${newVersion}`
+        
         await tools.runInWorkspace('git', ['commit', '-a', '-m', commitMessage.replace(/{{version}}/g, newVersion)])
 
         // now go to the actual branch to perform the same versioning
@@ -146,8 +147,6 @@ Toolkit.run(async (tools) => {
         await tools.runInWorkspace('git', ['checkout', currentBranch])
         await tools.runInWorkspace('npm', ['version', '--allow-same-version=true', '--git-tag-version=false', current])
         console.log('current:', current, '/', 'version:', version)
-        newVersion = execSync(`npm version --git-tag-version=false ${version}`).toString().trim().replace(/^v/, '')
-        newVersion = `${tagPrefix}${newVersion}`
         console.log(`::set-output name=newTag::${newVersion}`)
         try {
             // to support "actions/checkout@v1"
